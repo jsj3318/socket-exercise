420c13f4e1ddd5979e03e523762cd8c3c6e99415
조승주 <tmdwn3318@gmail.com>
2024-09-23 14:35:15 +0900

    완료

--- src/test/java/com/nhnacademy/server/runable/MessageServerTest.java
+++ src/test/java/com/nhnacademy/server/runable/MessageServerTest.java
@@ -19,6 +19,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 @Slf4j
@@ -40,16 +41,20 @@ class MessageServerTest {
     @Order(1)
     @DisplayName("constructor : port <= 0")
     void constructorTest1(){
-        //TODO#1-11 portt < 0  검증하는 코드를 작성하세요
-
+        //TODO#1-11 port < 0  검증하는 코드를 작성하세요
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+           new MessageServer(-1111);
+        });
     }
 
     @Test
     @Order(2)
-    @DisplayName("aready used port")
+    @DisplayName("already used port")
     void constructorTest2(){
         //TODO#1-12 이미 서버는 8888 port를 사용하고 있을 때 8888포트로 서버를 시작한다면 RuntimeException 발생하는지 검증 합니다.
-
+        Assertions.assertThrows(RuntimeException.class, ()->{
+           new MessageServer(8888);
+        });
     }
 
     @Test
@@ -60,15 +65,15 @@ class MessageServerTest {
             - host : localhost
             - port : 8888
          */
-        Socket client = null;
+        Socket client = new Socket("localhost", 8888);
 
         /*TODO#1-14 간단한 test client 구현
           - cleint 가 server에 메시지를 전송하기 위해서 PrintWriter 객체를 사용할 수 있도록 초기화 합니다.
           - server가 전송하는 데이터를 받기 위해서 BufferedReader 객체를 초기화 합니다.
          */
 
-        try(PrintWriter out = null;
-            BufferedReader in = null;
+        try(PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
         ){
             Assertions.assertAll(
                     ()->{
@@ -81,12 +86,18 @@ class MessageServerTest {
                     ()->{
                         // TODO#1-15  client가 server로 'java' message를  전송할 때 server가 client가 전송한 message를 응답하는지 검증하는 코드를 작성하세요
                         String message = "java";
-
+                        out.println(message);
+                        String actual = in.readLine();
+                        log.debug("actual:{}", actual);
+                        Assertions.assertEquals(message, actual);
                     },
                     ()->{
                         // TODO#1-16 '엔에이치엔아카데미' 검증하는 코드를 작성 하세요
                         String message = "엔에이치엔아카데미";
-
+                        out.println(message);
+                        String actual = in.readLine();
+                        log.debug("actual:{}", actual);
+                        Assertions.assertEquals(message, actual);
                     }
             );
         }
@@ -95,7 +106,7 @@ class MessageServerTest {
     @AfterAll
     static void tearDown(){
         // TODO#1-17 모든 테스트가 종료되면 server를 구동하고있는 thread에 interrupt()를 발생시켜 종료 합니다.
-
+        thread.interrupt();
     }
 
 }
